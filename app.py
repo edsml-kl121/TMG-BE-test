23c5e0e0194b926d0c33e7696fb6333895c653b5
Kandanai Leenutaphong <Kandanai.Leenutaphong@ibm.com>
2023-12-06 19:42:51 +0700

    d

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # This handles the GET request for the index route
     return render_template('index.html')
+
+@app.route('/data', methods=['POST'])
+def data():
+    # This handles the POST request for the /data route
+    return "I am connected to the Flask app"
 
 if __name__ == '__main__':
     app.run(debug=True)
